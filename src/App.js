6b30b58eb9f6c634c5c8cfa6eae6eb58819fe8fc
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-06-11 20:10:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Tictaktoe from "./Components/Tic-tak-toe";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="game">
+      <Tictaktoe />
+    </div>
+  );
+}
